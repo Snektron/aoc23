9447c47b44d79ed555186f4abc3592f88b8e4fc6
Robin Voetter <robin@voetter.nl>
2023-12-01 20:25:02 +0100

    a

--- flake.nix
+++ flake.nix
@@ -2,19 +2,31 @@
   description = "AOC23 flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
   };
 
   outputs = { self, nixpkgs }:
   let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
-  in {
+  in rec {
+    packages.${system}.algol68g = pkgs.algol68g.overrideAttrs (old: {
+      # gsl-config is needed on the path
+      nativeBuildInputs = old.nativeBuildInputs ++ [ pkgs.gsl ];
+    });
+
     devShells.${system}.default = pkgs.mkShell {
       name = "aoc23";
 
       nativeBuildInputs = with pkgs; [
-        nasm
+        packages.${system}.algol68g
+        gdb
+        postgresql_15
+        plotutils
+        gsl
+        gmp
+        mpfr
+        ncurses
       ];
     };
   };
