1c7aad6b60943e88f9e519fc22fbd323f8521959
Robin Voetter <robin@voetter.nl>
2023-12-02 13:35:47 +0100

    add a68toc

--- flake.nix
+++ flake.nix
@@ -10,16 +10,61 @@
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
   in rec {
-    packages.${system}.algol68g = pkgs.algol68g.overrideAttrs (old: {
-      # gsl-config is needed on the path
-      nativeBuildInputs = old.nativeBuildInputs ++ [ pkgs.gsl ];
-    });
+    packages.${system} = {
+      algol68g = pkgs.algol68g.overrideAttrs (old: {
+        # gsl-config is needed on the path
+        nativeBuildInputs = old.nativeBuildInputs ++ [ pkgs.gsl ];
+      });
+
+      a68toc = pkgs.callPackage ({
+        stdenv,
+        fetchFromGitHub,
+        which
+      }: stdenv.mkDerivation {
+        name = "a68toc";
+        version = "1.15";
+
+        src = fetchFromGitHub {
+          owner = "coolbikerdad";
+          repo = "Algol-68RS-Source";
+          rev = "ef25e71aa7d45f602ad464dd28059be3834db173";
+          hash = "sha256-81RcAoPqYZjQv5ODgf9uNahbbCUJ0qANnsP0Yt7DcEA=";
+        };
+
+        nativeBuildInputs = [ which ];
+
+        A68_NAMESEED = "nameseed";
+
+        installPhase = ''
+          mkdir -p $out
+          make \
+            PKGDIR=$out/share/algol68toc \
+            DOCDIR=$out/share/doc/algol68toc \
+            BINDIR=$out/bin \
+            LIBDIR=$out/lib \
+            INFODIR=$out/share/info \
+            MANDIR=$out/share/man/man1 \
+            INCDIR=$out/include/algol68 \
+            APPDIR=$out/share/applications \
+            DESTDIR=$out \
+            INSTALL=install \
+            install
+
+            substituteInPlace $out/bin/ca \
+              --replace "/usr/bin/a68toc" "$out/bin/a68toc" \
+              --replace "/usr/share/algol68toc" "$out/share/algol68toc" \
+              --replace "/usr/include/algol68" "$out/include/algol68" \
+              --replace "/usr/include" "$out/include"
+        '';
+      }) {};
+    };
 
     devShells.${system}.default = pkgs.mkShell {
       name = "aoc23";
 
       nativeBuildInputs = with pkgs; [
         packages.${system}.algol68g
+        packages.${system}.a68toc
         gdb
         postgresql_15
         plotutils
@@ -28,6 +73,8 @@
         mpfr
         ncurses
         hyperfine
+        linuxPackages_latest.perf
+        python310
       ];
     };
   };
